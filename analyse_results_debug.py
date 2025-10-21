dd68358e4e2858a5d84b45471f92dbdf1f7f280c
TrollDuLieu <135204150+Hung4865@users.noreply.github.com>
2025-10-22 02:41:44 +0700

    Add files via upload

--- analyse_results_debug.py
+++ analyse_results_debug.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 """
 Created on Thu Jul  5 12:02:41 2018
 
@@ -14,57 +15,119 @@ import json
 from scipy import stats
 import matplotlib.patheffects as pe
 import glob
+import os
+from multiprocessing.dummy import Pool as ThreadPool
+from functools import partial
+import multiprocessing as mp
+
 time = 1000000
-pathSimple ="exp_rev/"
+pathSimple = "exp_rev/"
 
 
 # =============================================================================
 # Computes the Response time by (App,User)
 # It takes into account the number of messages (mode) that it sends each (App,User)
 # =============================================================================
-def getRbyApp(df,dtmp):
-    myDeadlines = [487203.22, 487203.22,487203.22,474.51,302.05,831.04,793.26,1582.21,2214.64,374046.40,420476.14,2464.69,97999.14,2159.73,915.16,1659.97,1059.97,322898.56,1817.51,406034.73]
+def _compute_group_result(group_key, ids, id_stats, myDeadlines):
+    """Compute the result for a single (app,user) group.
 
-    dr = pd.DataFrame(columns=['app', 'user', 'avg','std','m','r','invalid','over']) #m - numero de mensajes enviados 
-    times = []
-    ixloc =0
-    for g in dtmp.keys():
-        ids = dtmp[g]
-        responses = []
-        messages = []
-        over = 0
-        #Firstly, it computes the mode in all the app,user transmissions
-        for i in ids:
-            messages.append(df[df.id==i].shape[0]) # number of messages send by the user
-            
-        #Requests with a inferior number of messages are filtered
-        msg = np.array(messages)
-        mode = stats.mode(msg).mode[0] 
-        
-        #Secondly, if each transmission has the same mode then the time is storaged
-        invalid =0
-        for i in ids:
-            dm = df[df.id==i]
-            if mode == dm.shape[0]:
-                r =dm['time_out'].max()-dm['time_emit'].min()
-                if r <= myDeadlines[g[0]]:
-#                if True:
-                       responses.append(r)
-                       times.append(dm['time_emit'].min())
-                else:
-                       over+=1
+    Inputs:
+    - group_key: tuple (app, user)
+    - ids: list of ids belonging to the group
+    - id_stats: dict mapping id -> (count, min_emit, max_out)
+    - myDeadlines: list of deadlines by app index
+
+    Returns: tuple (app, user, avg, std, mode, r_array, invalid, over, times_list)
+    """
+    responses = []
+    times_local = []
+    messages = []
+    over = 0
+    for i in ids:
+        stats_i = id_stats.get(i)
+        if stats_i is None:
+            # missing id
+            messages.append(0)
+        else:
+            messages.append(stats_i[0])
+
+    msg = np.array(messages)
+    # stats.mode returns a ModeResult; we guard for empty
+    if len(msg) == 0:
+        mode = 0
+    else:
+        mode = stats.mode(msg, keepdims=True).mode[0]
+
+    invalid = 0
+    for i in ids:
+        stats_i = id_stats.get(i)
+        if stats_i is None:
+            invalid += 1
+            continue
+        cnt, min_emit, max_out = stats_i
+        if mode == cnt:
+            r = max_out - min_emit
+            if r <= myDeadlines[group_key[0]]:
+                responses.append(r)
+                times_local.append(min_emit)
             else:
-                invalid+=1
-        
-        resp = np.array(responses)
-   
-        avg = resp.mean()
-        dsv = resp.std()
-        dr.loc[ixloc] = [g[0],g[1],avg,dsv,mode,resp,invalid,over]
-        ixloc+=1
-        print g,"\t",len(dtmp[g]),"\t",invalid,"\t",over
-           
-    return dr,times
+                over += 1
+        else:
+            invalid += 1
+
+    if len(responses) > 0:
+        resp_arr = np.array(responses)
+        avg = resp_arr.mean()
+        dsv = resp_arr.std()
+    else:
+        resp_arr = np.array([])
+        avg = np.nan
+        dsv = np.nan
+
+    return (group_key[0], group_key[1], avg, dsv, mode, resp_arr, invalid, over, times_local)
+
+
+def getRbyApp(df, dtmp, use_mp=True):
+    """Compute response metrics per (app,user) group more efficiently.
+
+    Optimizations:
+    - Precompute per-id aggregates (count, min_emit, max_out) once.
+    - Process groups in parallel using multiprocessing Pool (if use_mp True).
+    """
+    myDeadlines = [487203.22, 487203.22, 487203.22, 474.51, 302.05, 831.04, 793.26, 1582.21, 2214.64, 374046.40, 420476.14, 2464.69, 97999.14, 2159.73, 915.16, 1659.97, 1059.97, 322898.56, 1817.51, 406034.73]
+
+    # Precompute per-id statistics: count, min(time_emit), max(time_out)
+    id_groups = df.groupby('id')
+    id_stats = {}
+    for id_val, grp in id_groups:
+        id_stats[id_val] = (grp.shape[0], grp['time_emit'].min(), grp['time_out'].max())
+
+    items = list(dtmp.items())
+    results = []
+
+    if use_mp and len(items) > 0:
+        # Use as many workers as CPU cores, but cap to avoid oversubscription
+        workers = min( mp.cpu_count(), max(1, len(items)) )
+        with mp.Pool(processes=workers) as pool:
+            func = partial(_compute_group_result, id_stats=id_stats, myDeadlines=myDeadlines)
+            # map with starmap-like behavior
+            async_results = [pool.apply_async(func, args=(k, v)) for k, v in items]
+            for ar in async_results:
+                results.append(ar.get())
+    else:
+        for k, v in items:
+            results.append(_compute_group_result(k, v, id_stats, myDeadlines))
+
+    # Build DataFrame
+    dr = pd.DataFrame(columns=['app', 'user', 'avg', 'std', 'm', 'r', 'invalid', 'over'])
+    times = []
+    for ixloc, res in enumerate(results):
+        app, user, avg, dsv, mode, resp_arr, invalid, over, times_local = res
+        dr.loc[ixloc] = [app, user, avg, dsv, mode, resp_arr, invalid, over]
+        times.extend(times_local)
+        print(app, "\t", len(dtmp[(app, user)]), "\t", invalid, "\t", over)
+
+    return dr, times
 
 def drawBoxPlot_User_App(dr,app):
     fig, ax = plt.subplots()
@@ -99,8 +162,8 @@ def drawBoxPlot_Both_USER(app,dr,drILP):
     data_a=dr[dr.app==app].r.values
     data_b=drILP[drILP.app==app].r.values
     ticks = list(np.sort(dr[dr.app==app].user.unique()))
-    bpl = plt.boxplot(data_a, positions=np.array(xrange(len(data_a)))*2.0-0.4, sym='', widths=0.6)
-    bpI = plt.boxplot(data_b, positions=np.array(xrange(len(data_b)))*2.0+0.4, sym='', widths=0.6)
+    bpl = plt.boxplot(data_a, positions=np.array(list(range(len(data_a))))*2.0-0.4, sym='', widths=0.6)
+    bpI = plt.boxplot(data_b, positions=np.array(list(range(len(data_b))))*2.0+0.4, sym='', widths=0.6)
     set_box_color(bpl, '#5ab4ac') # colors are from http://colorbrewer2.org/
     set_box_color(bpI, '#d8b365')
     # draw temporary red and blue lines and use them to create a legend
@@ -108,7 +171,7 @@ def drawBoxPlot_Both_USER(app,dr,drILP):
     plt.plot([], c='#d8b365', label='ILP') 
     plt.legend()
     
-    plt.xticks(xrange(0, len(ticks) * 2, 2), ticks)
+    plt.xticks(list(range(0, len(ticks) * 2, 2)), ticks)
     plt.xlim(-2, len(ticks)*2)
     #plt.ylim(0, 10000)
 #    plt.ylim(00, 1000)
@@ -127,8 +190,8 @@ def drawBoxPlot_App(dar,darILP,labeldar="Partition",labelILP="ILP"):
     data_b=darILP.r.values
     ticks = list(np.sort(dar.app.unique()))
       
-    bpl = plt.boxplot(data_a, positions=np.array(xrange(len(data_a)))*2.0-0.4, sym='', widths=0.6)
-    bpI = plt.boxplot(data_b, positions=np.array(xrange(len(data_b)))*2.0+0.4, sym='', widths=0.6)
+    bpl = plt.boxplot(data_a, positions=np.array(list(range(len(data_a))))*2.0-0.4, sym='', widths=0.6)
+    bpI = plt.boxplot(data_b, positions=np.array(list(range(len(data_b))))*2.0+0.4, sym='', widths=0.6)
     set_box_color(bpl, '#5ab4ac') # colors are from http://colorbrewer2.org/
     set_box_color(bpI, '#d8b365')
     # draw temporary red and blue lines and use them to create a legend
@@ -136,7 +199,7 @@ def drawBoxPlot_App(dar,darILP,labeldar="Partition",labelILP="ILP"):
     plt.plot([], c='#d8b365', label=labelILP) 
     plt.legend()
     
-    plt.xticks(xrange(0, len(ticks) * 2, 2), ticks)
+    plt.xticks(list(range(0, len(ticks) * 2, 2)), ticks)
     plt.xlim(-2, len(ticks)*2)
     #plt.ylim(50, 400)
     #plt.ylim(0, 10000)
@@ -156,7 +219,16 @@ NEW = False
 # =============================================================================
 # ---- đọc nhiều file Partition ----
 all_files = glob.glob(path + "_*.csv")   # ví dụ Results_RND_FAIL__1000000_0.csv, _1.csv ...
-df = pd.concat([pd.read_csv(f) for f in all_files], ignore_index=True)
+if len(all_files) == 0:
+    df = pd.DataFrame()
+else:
+    # Read CSVs in parallel using threads (I/O bound)
+    max_workers = min(8, len(all_files))
+    pool = ThreadPool(max_workers)
+    dfs = pool.map(pd.read_csv, all_files)
+    pool.close()
+    pool.join()
+    df = pd.concat(dfs, ignore_index=True, sort=False)
 
 dtmp = df[df["module.src"]=="None"].groupby(['app','TOPO.src'])['id'].apply(list)
 dr, timeC = getRbyApp(df, dtmp)
@@ -164,7 +236,15 @@ drAll = getAllR(dr)
 
 # ---- đọc nhiều file ILP ----
 all_files_ilp = glob.glob(pathILP + "_*.csv")  # ví dụ Results_RND_FAIL_ILP_1000000_0.csv ...
-dfILP = pd.concat([pd.read_csv(f) for f in all_files_ilp], ignore_index=True)
+if len(all_files_ilp) == 0:
+    dfILP = pd.DataFrame()
+else:
+    max_workers = min(8, len(all_files_ilp))
+    pool = ThreadPool(max_workers)
+    dfs_ilp = pool.map(pd.read_csv, all_files_ilp)
+    pool.close()
+    pool.join()
+    dfILP = pd.concat(dfs_ilp, ignore_index=True, sort=False)
 
 dtmp2 = dfILP[dfILP["module.src"]=="None"].groupby(['app','TOPO.src'])['id'].apply(list)
 drILP, timeILP = getRbyApp(dfILP, dtmp2)
@@ -272,6 +352,8 @@ dILP = dILP.resample('500s').agg(dict(QTY='sum'))
 QTYILP = dILP.QTY.values
 
 
+
+
 #####ALERTA
 #np.save("exp_final3/QTYC.npy",QTYC)
 #np.save("exp_final3/QTYFailsCR.npy",QTYFailsCR)
@@ -326,7 +408,6 @@ QTYILP = dILP.QTY.values
 
 
 
-
 # =============================================================================
 # Boxplot matriz of each app - gtw/user
 # =============================================================================
@@ -335,19 +416,19 @@ def drawBoxPlot_Both_USER_ax(app,dr,drILP,ax):
     data_a=dr[dr.app==app].r.values
     data_b=drILP[drILP.app==app].r.values
     ticks = list(np.sort(dr[dr.app==app].user.unique()))
-    bpl = ax.boxplot(data_a, positions=np.array(xrange(len(data_a)))*2.0-0.4, sym='', widths=0.55,
+    bpl = ax.boxplot(data_a, positions=np.array(list(range(len(data_a))))*2.0-0.4, sym='', widths=0.55,
                      whiskerprops = dict(linewidth=2),
                     boxprops = dict(linewidth=2),
                      capprops = dict(linewidth=2),
                     medianprops = dict(linewidth=2))
-    bpI = ax.boxplot(data_b, positions=np.array(xrange(len(data_b)))*2.0+0.4, sym='', widths=0.55,
+    bpI = ax.boxplot(data_b, positions=np.array(list(range(len(data_b))))*2.0+0.4, sym='', widths=0.55,
                         whiskerprops = dict(linewidth=2),
                     boxprops = dict(linewidth=2),
                      capprops = dict(linewidth=2),
                     medianprops = dict(linewidth=2))
     set_box_color(bpl, '#a6bddb')
     set_box_color(bpI, '#e34a33')
-    ax.get_xaxis().set_ticks(xrange(0, len(ticks) * 2, 2))
+    ax.get_xaxis().set_ticks(list(range(0, len(ticks) * 2, 2)))
     ax.set_xticklabels(ticks)
     ax.set_xlim(-2, len(ticks)*2)
     ax.plot([], c='#a6bddb', label="Partition",linewidth=3)
@@ -366,6 +447,8 @@ axlist[3][2].set_xlabel('IoT devices (Gateways id.)',fontsize=14)
 axlist[1][0].set_ylabel('Response time (ms)',fontsize=14)
 axlist[1][0].yaxis.set_label_coords(-0.4, 0)
 ax.tick_params(labelsize=12)
+
+
 plt.savefig('Boxplot.pdf', format='pdf', dpi=600)
 plt.show()
 
